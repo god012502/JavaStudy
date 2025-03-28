0f9f6adf226dc6bf751834bc6789c630fda49bb9
god012502 <god012502@naver.com>
2025-03-28 16:42:24 +0900

    학원

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("2.Hello Eclipse");
 		System.out.println("3.Github연동완료");
 		System.out.print("4.집에서 작업");
+		System.out.println("5.학원에서 작업");
 		//sysout ctrl+스페이스 바 - println 자동완성
 		//ctrl+shift+[+,-] 폰트 크기 확대, 축소
 		//라인복사 - ctrl+alt+방향키
